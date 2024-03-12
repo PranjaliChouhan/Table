901a4972d279cb462a1edc32c9c0918542f94be1
PranjaliChouhan <pranjalichouhan4@gmail.com>
2024-03-12 16:56:33 +0530

    update

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let myData=[
+let myData = [
     {
         firstname: 'John',
         lastname: 'Cena',
         age: 21,
-        fullname:function(){
+        fullname: function () {
             return this.firstname + ' ' + this.lastname;
         }
     },
@@ -11,7 +11,7 @@ let myData=[
         firstname: 'rohan',
         lastname: 'Cena',
         age: 21,
-        fullname:function(){
+        fullname: function () {
             return this.firstname + ' ' + this.lastname;
         }
     },
@@ -19,7 +19,7 @@ let myData=[
         firstname: 'rahul',
         lastname: 'Cena',
         age: 21,
-        fullname:function(){
+        fullname: function () {
             return this.firstname + ' ' + this.lastname;
         }
     },
@@ -27,29 +27,49 @@ let myData=[
         firstname: 'laddu',
         lastname: 'Cena',
         age: 21,
-        fullname:function(){
+        fullname: function () {
             return this.firstname + ' ' + this.lastname;
         }
     }
-]
-let listholder=document.querySelector("#listHolder");
-let firstname=document.querySelector("#firstname");
-let lastname=document.querySelector("#lastname");
-let age=document.querySelectors("#age");
-let btnsubmit=document.querySelector("#btnsubmit");
+];
 
+let listholder = document.querySelector("#listHolder");
+let firstname = document.querySelector("#firstname");
+let lastname = document.querySelector("#lastname");
+let age = document.querySelector("#age");
+let btnsubmit = document.querySelector("#btnsubmit");
 
-
-myData.forEach((item,index)=> {
-    let tr=document.createElement("tr");
-    tr.innerHTML=`
-    <td>${index+1}</td>
-    <td>${item.firstname}</td>
-   <td>${item.lastname}</td
-   <td>${item.age}</td> `
-   ;
-   listholder.appendChild(tr);
-
-    
+btnsubmit.addEventListener("click", function () {
+    if (firstname.value.trim() === "" || lastname.value.trim() === "" || age.value.trim() === "") {
+        alert("Please fill out the form completely");
+    } else {
+        let copyData = [...myData];
+        let newEntry = {
+            firstname: firstname.value.trim(),
+            lastname: lastname.value.trim(),
+            age: age.value.trim(),
+            fullname: function () {
+                return this.firstname + " " + this.lastname;
+            }
+        };
+        copyData.push(newEntry);
+        console.log(copyData);
+        refreshList(copyData);
+    }
 });
 
+function refreshList(data) {
+    listholder.innerHTML = "";
+    data.forEach((item, index) => {
+        let tr = document.createElement("tr");
+        tr.innerHTML = `
+        <td>${index + 1}</td>
+        <td>${item.firstname}</td>
+        <td>${item.lastname}</td>
+        <td>${item.age}</td>`;
+        listholder.appendChild(tr);
+    });
+}
+
+// Initial rendering of the list
+refreshList(myData);
